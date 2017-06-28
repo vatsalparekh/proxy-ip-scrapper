eb274b34ddf7ae99ba3637beb520dfb6d6b54dd0
Yashashvi Dave <yashashvidave@gmail.com>
2017-06-28 18:55:40 +0530

    Create urlconfig.py

---
+++ proxy-ip-scrapper-django-api/scraper/urlconfig.py
@@ -0,0 +1,25 @@
+###################FOR DEMO PURPOSE####################
+urls = ["https://free-proxy-list.net"]
+
+# urls = ["https://free-proxy-list.net","https://free-proxy-list.net/uk-proxy.html",
+#       "https://www.us-proxy.org","https://www.sslproxies.org","https://premproxy.com/list",
+#       "http://nntime.com/","https://hidemy.name/en/proxy-list/","http://free-proxy.cz",
+#       "https://hidester.com/proxylist/","https://nordvpn.com/free-proxy-list/",
+#       "https://www.hide-my-ip.com/proxylist.shtml","http://www.httptunnel.ge/ProxyListForFree.aspx"]
+
+
+
+id_tags_name = ["proxylisttable", "proxylisttable", "proxylisttable", "proxylisttable", "proxylist", "proxylist",
+                "", "proxy_list", "", "", "sort-list", "ctl00_ContentPlaceHolder1_GridViewNEW"]
+
+class_name = ["", "", "", "", "", "", "proxy__t", "", "table table-hover",
+              "table table-bordered table-hover table-hover proxy-list-table", "", ""]
+
+ip_seq = [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1]
+port_seq = [1, 1, 1, 1, 20, 20, 1, 1, 2, 2, 1, 20, 2, 2, 2]
+country_seq = [3, 3, 3, 3, 3, 4, 2, 3, 3, 0, 2, 20, 4, 4, 4]
+anon_seq = [4, 4, 4, 4, 1, 2, 5, 6, 7, 20, 5, 20, 3, 3, 5]
+https_seq = [6, 6, 6, 6, 20, 20, 4, 2, 4, 3, 4, 20, 6, 6, 6]
+pages = [15, 5, 10, 5, 20, 18, 36, 9, 5, 15, 18, 1, 2, 6, 2]
+
+
