4e2a3b7e0cf7775b1d512a64bbc24af1e787f205
Vatsal Parekh <vatsalparekh@outlook.com>
2017-07-01 19:36:39 +0530

    Modified celery.py

--- proxy_ip_scrapper/proxy_ip_scrapper/celery.py
+++ proxy_ip_scrapper/proxy_ip_scrapper/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 class Config:
-    # Configuration for celery tasks::
+    # Config class which include all configurations for celery
 
     BROKER_URL = 'amqp://guest:guest@localhost:5672//'
     CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
@@ -21,13 +21,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proxy_ip_scrapper.settings')
 # Create instance of Celery
 app = Celery('proxy_ip_scrapper')
 
-
 # This method load configuration from a configuration object.
-# Any configuration that was previously set will be reset when this method is called
+# Any configuration that was previously set will be reset,
+# when this method is called
 # So additional configuration should be written after this method
 app.config_from_object(Config)
-# Here Config is a class which include all configurations for celery
-
 
 # To find tasks.py file from apps
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
