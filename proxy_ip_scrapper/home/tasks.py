b48389e694dc9199b1dc1562d360bb05442ebdd4
YJDave <yashashvidave@gmail.com>
2017-07-01 16:29:40 +0530

    Add celery, tasks, ScraperFunctions

---
+++ proxy_ip_scrapper/home/tasks.py
@@ -0,0 +1,18 @@
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+
+from .scraperfunc import call_browser
+from .urlconfig import urls
+
+logger = get_task_logger(__name__)
+
+@periodic_task(
+    run_every=(crontab(minute='*/5')),             ###To CHANGE TIME INTERVAL####
+    name="scraping data and pushing to database",
+    ignore_result=True)
+def main():
+    url_index = 0
+    while (url_index < len(urls)):
+        call_browser(url_index)
+        url_index += 1
---
+++ proxy_ip_scrapper/proxy_ip_scrapper/celery.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+class Config:
+	#Configuration for celery tasks::
+
+	BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+	CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
+	CELERY_ACCEPT_CONTENT = ['application/json']
+	CELERY_TASK_SERIALIZER = 'json'
+	CELERY_RESULT_SERIALIZER = 'json'
+	CELERY_TIMEZONE = 'Asia/Calcutta'
+
+
+
+#Set default configuration module name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proxy_ip_scrapper.settings')
+
+
+#Create instance of Celery
+app = Celery('proxy_ip_scrapper')
+
+
+#This method load configuration from a configuration object.
+#Any configuration that was previously set will be reset when this method is called
+#So additional configuration should be written after this method
+app.config_from_object(Config)
+#Here Config is a class which include all configurations for celery
+
+
+#To find tasks.py file from apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
